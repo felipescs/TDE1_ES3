a7d8f0b33ea399114482d12b0cc98b88c0b1ea91
felipescs <42879548+felipescs@users.noreply.github.com>
2018-08-31 20:19:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+def soma(a,b):
+	soma = a + b
+	return soma
+
+def subtra(a,b):
+	subtra = a - b
+	return subtra
+
+def mult(a,b):
+	mult = a * b
+	return mult
---
+++ test_pytestes.py
@@ -0,0 +1,18 @@
+import pytest
+from app import soma
+from app import subtra
+from app import mult
+
+def test_soma1():
+	assert soma(1,1)==2
+	assert soma(-1,-2)==-3
+	assert soma(-1,3)==2
+	assert soma(1.2,2.3)==3.5
+	assert soma(2,2)==4
+
+def test_subtra1():
+	assert subtra(1,1)==0
+	assert subtra(3.2,2.2)==1
+
+def test_mult():
+	assert mult(1,2)==2
